2cee9c28c727b793a784a882dfd78f7bdab1839b
Nolan Foster <nolanfoster@gmail.com>
2016-06-19 12:19:29 -0400

    initial commit

---
+++ mobile/src/main/java/me/nolanfoster/open_iot_android/MainActivity.java
@@ -0,0 +1,24 @@
+package me.nolanfoster.open_iot_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+}
